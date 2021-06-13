980db0d0572b9e19b26f48525ad57437da3faf8d
gneris177 <gneris177@gmail.com>
2021-06-13 15:00:20 -0300

    criando controller users

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const controller = require('../controllers/loginController');
+const controller = require('../controllers/usersController');
 
 router.post('/login', controller.login)
 
--- src/routes/registerRouter.js
+++ src/routes/registerRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const controller = require('../controllers/registerController');
+const controller = require('../controllers/usersController');
 
 router.post('/register', controller.register);
 
